4466638860a21ee2e0f7ec4abfb4991a5d2d70ef
tak <re795h@gmail.com>
2020-07-14 11:07:13 +0900

    add nng by c

--- README.md
+++ README.md
@@ -1,22 +1,48 @@
 # bench-tcp-libs
-Benchmarking tcp libs like ZeroMQ and Nanomsg by c and zig
+Benchmarking tcp libs like ZeroMQ and nng by c and zig
 
 # Install
 ```
+// Install ZeroMQ
 sudo apt-get install libczmq-dev -y
+
+// Install build tools for nng
+sudo apt install cmake -y
+sudo apt install ninja-build -y
+
+// install nng
+wget https://github.com/nanomsg/nng/releases/tag/v1.3.0/nng-1.3.0.tar.gz
+tar -zxvf nng-1.3.0.tar.gz
+mkdir build
+cd build
+cmake -G Ninja ..
+ninja
+ninja test -> 1 test failed in my case: 41 - resolv (Failed)
+ninja install
+ldconfig
 ```
 
 # Benchmarking
 ## ZeroMQ by c
-```
+```bash
 gcc -o c/bench-zeromq c/bench-zeromq.c -lczmq
 ./c/bench-zeromq
 BenchmarkZeroMQ-C:   76280   7628 loops/s   131095 ns/op
 ```
 
 ## ZeroMQ by zig
-```
+```bash
 zig build -Drelease-fast=true
 ./zig-cache/bin/bench-tcp
 BenchmarkZeroMQ-Zig:   48232   4823 loops/s   207331 ns/op
+```
+
+## nng by c
+```bash
+gcc c/bench-nng.c -o c/bench-nng -lnng -pthread
+./c/bench-nng server tcp://127.0.0.1:5560 & server=$! && sleep 1 # run server
+./c/bench-nng
+BenchmarkNNG-C:   116917   11691 loops/s   85530 ns/op
+
+kill $server # stop server
 ```
---
+++ c/bench-nng.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include <nng/nng.h>
+#include <nng/protocol/pipeline0/pull.h>
+#include <nng/protocol/pipeline0/push.h>
+
+#define Server "server"
+#define Client "client"
+
+nng_socket client_sock; // for client
+
+// 1400 length
+char* msg = "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello";
+
+int log_error(const char *func, int rv) {
+  fprintf(stderr, "%s: %s\n", func, nng_strerror(rv));
+  return 1;
+}
+
+int server(const char *url) {
+  nng_socket sock;
+  int rv;
+
+  if ((rv = nng_pull0_open(&sock)) != 0) {
+    return log_error("nng_pull0_open", rv);
+  }
+  if ((rv = nng_listen(sock, url, NULL, 0)) != 0) {
+    return log_error("nng_listen", rv);
+  }
+
+  printf("listening: %s\n", url);
+
+  for (;;) {
+    char *buf = NULL;
+    size_t sz;
+    if ((rv = nng_recv(sock, &buf, &sz, NNG_FLAG_ALLOC)) != 0) {
+      return log_error("nng_recv", rv);
+    }
+    // printf("server: recv \"%s\"\n", buf);
+    nng_free(buf, sz);
+  }
+
+  return 0;
+}
+
+int bench(const unsigned char *name, double benchtime) {
+  clock_t start_t = clock();
+
+  int loops = 0;
+  while (clock() < start_t + benchtime) {
+    // printf("client: send \"%s\"\n", msg);
+    if (nng_send(client_sock, msg, strlen(msg)+1, 0) != 0) {
+      printf("faild to send");
+      return 1;
+    }
+    loops += 1;
+  }
+
+  int loop_per_s = loops * CLOCKS_PER_SEC / benchtime;
+  int time_per_op = benchtime / loops * 1000;
+  printf("%s:   %d   %d loops/s   %d ns/op\n", name, loops, loop_per_s, time_per_op);
+  return 0;
+}
+
+int clinet(const char *url) {
+  int rv;
+
+  if ((rv = nng_push0_open(&client_sock)) != 0) {
+    return log_error("nng_push0_open", rv);
+  }
+  if ((rv = nng_dial(client_sock, url, NULL, 0)) != 0) {
+    return log_error("nng_dial", rv);
+  }
+}
+
+int main(int argc, char **argv) {
+  // Start server
+  if ((argc > 1) && (strcmp(Server, argv[1]) == 0)) {
+    return (server("tcp://127.0.0.1:5560"));
+  }
+
+  // client connect to server
+  if (clinet("tcp://127.0.0.1:5560") > 0) {
+    return 1;
+  }
+
+  double benchtime = 10 * CLOCKS_PER_SEC; // 10s
+  // double benchtime = CLOCKS_PER_SEC; // 1s
+  if (bench("BenchmarkNNG-C", benchtime) > 0) {
+    return 1;
+  }
+
+  nng_close(client_sock);
+  return 0;
+}
